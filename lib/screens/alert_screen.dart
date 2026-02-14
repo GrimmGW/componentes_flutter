b5dc00b9f7dde37719946f149e6f522601fe9296
GrimmGW <hectorl1805@gmail.com>
2026-02-13 21:05:33 -0400

    cambiando el color

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -35,7 +35,7 @@ class AlertScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pantalla de Alertas'),
         centerTitle: true,
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.indigoAccent,
       ),
       body: Center(
          child: FilledButton(
